1e7ec8fb071434f1afa63b50b9e2c785f95ae057
Triangulation5 <joshua.jomet@gmail.com>
2026-05-10 15:25:00 -0400

    Made a good configuration for you

---
+++ lua/chadrc.lua
@@ -0,0 +1,45 @@
+-- This file needs to have same structure as nvconfig.lua 
+-- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
+-- Please read that file to know all available options :( 
+
+---@type ChadrcConfig
+local M = {}
+
+M.base46 = {
+	theme = "flexoki",
+    -- transparency = true,
+
+	-- hl_override = {
+	-- 	Comment = { italic = true },
+	-- 	["@comment"] = { italic = true },
+	-- },
+}
+
+M.nvdash = { load_on_startup = true }
+-- M.ui = {
+--       tabufline = {
+--          lazyload = false
+--      }
+-- }
+
+M.ui = {
+    statusline = {
+        theme = "minimal", -- default/vscode/vscode_colored/minimal
+        separator_style = "round", -- default/round/block/arrow
+    },
+
+     cmp = {
+      lspkind_text = true,
+      style = "atom_colored", -- default/flat_light/flat_dark/atom/atom_colored
+      format_colors = { 
+         tailwind = false,
+      },
+ },
+}
+
+ M.cheatsheet = {
+   theme = "simple", -- simple/grid
+ }
+
+
+return M
---
+++ lua/options.lua
@@ -0,0 +1,58 @@
+require "nvchad.options"
+
+vim.o.mouse = ""
+
+vim.o.shortmess = vim.o.shortmess .. "I"
+
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.numberwidth = 4
+
+vim.o.signcolumn = "yes"
+
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+
+vim.o.smartindent = true
+vim.o.breakindent = true
+
+vim.o.list = true    
+
+vim.o.smartcase = true
+vim.o.hlsearch = false
+vim.o.ignorecase = true
+
+vim.o.background = "dark"
+vim.o.guicursor = "a:block"
+
+vim.o.updatetime = 50
+vim.o.timeoutlen = 150
+
+vim.o.scrolloff = 8
+vim.o.sidescrolloff = 8
+
+vim.o.winborder = "none"
+vim.o.clipboard = "unnamedplus"
+
+vim.o.completeopt = "menuone,noselect"
+
+vim.o.conceallevel = 0
+vim.o.pumheight = 10
+vim.o.pumblend = 0
+
+vim.o.winblend = 0
+vim.o.swapfile = false
+vim.o.splitkeep = "screen"
+
+vim.o.ruler = false
+
+vim.o.title = true
+vim.o.titlestring = "nvim"
+vim.o.titlelen = 0
+
+-- add yours here!
+
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
