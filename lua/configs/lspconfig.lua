76afd687a24253336b7765cfd1707936ee497425
Triangulation5 <joshua.jomet@gmail.com>
2026-05-11 17:57:48 -0400

    Configured lspconfig.lua

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,7 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "vtsls", "pyright", "ruff", "gopls", "marksman" }
+local servers = { "vtsls", "pyright", "ruff", "gopls", "marksman", "tinymist" }
+-- local servers = { "html", "cssls", "lua_ls" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
